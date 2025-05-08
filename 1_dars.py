1e700cad11034b5ea36452f727b626adce97ae62
muhammadazizdev007 <mannobjonovmuhammadaziz7@gmail.com>
2025-05-08 18:34:42 +0500

    initial commit

---
+++ 1_dars.py
@@ -0,0 +1,80 @@
+# input() - bu foydalnuvchidan qiymatni olish funksiya
+# o'zgaruvchi = o'zida ma'lumot saqlab turuvchi nom
+
+# int = bu son turdagi ma'lumot turi
+
+# str = matn ko'rinishadagi ma'lumot turi
+
+
+
+# matn = input ("Matinni kiriting: ")
+
+# teskari_matn = " "
+# for harf in matn:
+
+#     teskari_matn = harf + teskari_matn
+# print ("Teskari matn:" , teskari_matn)
+
+# matn =input("So'zlar kiriting: ")
+
+# uzun_soz =" "
+
+# for soz in matn.split():
+#     if len(soz) > len(uzun_soz):
+#         uzun_soz = soz
+# print(uzun_soz)
+
+
+
+# yosh = input("salom yoshingizni kiriting: ")
+
+# if yosh.isdigit():
+#     yosh = int(yosh)
+# else:
+#     yosh = int(input("matn emas son kiriting: "))
+
+# if 18 <= yosh and yosh <= 33:
+#     print("siz armiyaga yaroqlisiz")
+
+# else:
+#     print("siz armiyaga yaroqsizsiz")
+
+
+
+# print("salom,", ism, "!")
+
+# ish = input("Ishlaring yaxshimi ?").lower()
+
+# if ish == 'xa':
+#     print("Unda yaxshi !")
+# else:
+#     ish = input("Nima uchun unday ?")
+
+# print("Suhbatingiz uchun rahmat !")
+
+
+
+# unlilar = "aAeEiIoOuUo'`g'G'"
+
+# matn = input("Matn kiriting: ")
+
+# harf = []
+# topilgan_unlilar = []
+
+
+# for harf in matn:
+#     if harf in unlilar:
+#         topilgan_unlilar.append(harf)
+
+# print("Matndagi unli harflar:", "".join(topilgan_unlilar))
+# print("Jami unli harflar soni:", len(topilgan_unlilar))
+
+
+avto = ["volvo", "audi", "bugatti"] #royxat 
+# print(avto[2])
+
+# for i in range(len(avto)):
+#     avto[i] = avto[i].title()
+#     print(avto[i])
+
+
---
+++ hello-world.py
@@ -0,0 +1,122 @@
+# print ni nimaligini o'rganish
+# ism = "Ali"
+# yosh = 17
+# print(ism)
+# print(yosh)
+
+# vazifa bajarildi
+# radius = 5
+# pi = 3.14159
+# aylana_yuzi = pi * radius**2
+# print("Radiusi" , radius, "ga teng aylananing yuzi", aylana_yuzi) 
+# javobi:Radiusi 5 ga teng aylananing yuzi 78.53975
+
+# ism = "Ali"
+# shahar = "Andijon"
+# viloyat = 'Andijon'
+# matn = "mening yuragim 🖤"
+# print(matn)
+
+# String ustida amallar
+# ism = 'Ali'
+# print("mening ismim " + ism )
+
+# ism = 'Ali'
+# familiya = 'Alimov'
+# print(ism + familiya)
+# print(ism + ' ' + familiya)
+
+# f - string 
+# ism = 'Ali'
+# familiya = 'Alimov'
+# ism_sharif = f"{ism} {familiya}"
+# print(ism_sharif)
+
+# ism = 'Ali'
+# familiya = "007"
+# print(f"Salom ,mening ismim {familiya} , {ism} {familiya}")
+
+# Maxsus belgilar
+# print("salom do'stim")
+# print('Hello \tWorld!')
+# print('Hello \nWorld!')
+
+# String metodlar
+# ism = 'Ali'
+# familiya = 'Alimov'
+# ism_sharif = f"{ism} {familiya}"
+# print(ism_sharif. upper ())
+# input haqida 
+# ism = input("ismingiz nima")
+# print("Assalomu alekum," +  ism)
+
+# ism = input("Ismingiz nima? \n>>>")
+# print("Assalomu alekum" + ism. title()) #so'zlarni bosh harini kotta qilish
+
+# ism = 'kocha='
+# nomi = 'bogbon'
+# print(f"{ism} {nomi} ")
+
+# ism = 'mahalla'
+# nomi = '"sogbon"'
+# print = (f" {ism} {nomi}")
+
+# ism = 'bogbon kochasi, Sagbon mahallasi'
+# Nomi = 'Bodomzor tumani, Samarqand viloyati'
+# print(f"{ism} {Nomi}" )
+# kocha = 'Eshki Shahar'
+# mahalla = 'Karim Ahmaidy'
+# tuman = 'Andijon'
+# print(kocha+ "kochasi," mahalla + "Karim Axmadiy," tuman + "Andijon" )
+
+# print("Iltimos quyidagi malumotlarni kiriting:")
+# kocha = input("Kochangiz")
+# mahalla = input("Mahallangiz")
+# tuman = input("Tumaninggiz")
+# print(kocha + " ko'chasi, " + mahalla + " mahallasi, " + \
+#       tuman + " tumani, ")   
+
+# print("Iltimos quydagi malumotni kiriting")
+# yashash_joyi = input("Yashash joyingiz") 
+# manzil = input("Manzilingiz")
+# shahar = input("Shaharingiz")
+# print(yashash_joyi + "Yashashjoyingiz," + shahar +"Shaharingiz," + manzil+"Manzilingiz,") 
+
+ # ^Birinchi yaratgan dasturim ^
+
+
+# Raqamlar bilan ishlash
+
+# a = 20
+# b = 5.5
+
+# aholi_soni = 7_555_777_594
+# print("Aholi soni" , aholi_soni)
+
+# t_yil = int(input("Tug'ilgan yilingizni kiritung: "))
+# yosh = 2025 - t_yil 
+# print("Siz", yosh, "yoshda ekansiz")
+# yosh = int(input("Yoshingizni kiriting: "))
+# yil = 2025 - yosh
+# print("Siz", yil, "Sizning yilingiz")
+
+# son = int(input("Istalgan son kiriting\n"))
+# kvadrat = son ** 2
+# kub = son ** 3
+# print(f"{son} ning kubi {kub} ga teng")
+# print(son , "ning kvadrati" ,kvadrat , "ga teng")
+
+# soat = int(input("istalgan son kiriting: "))
+# minut = 60
+# natija = soat * minut
+# print(natija , " daqiqa - " , soat, " soat")
+
+# sekund = int(input("Sekund kiriting: "))
+
+
+# soat = sekund // 3600
+# qolgan_sekund = sekund % 3600
+# minut = qolgan_sekund // 60
+# qolgan_sekund = qolgan_sekund % 60
+
+# print(soat, " soatu ", minut, " minut va ", qolgan_sekund, " soniya bo'ldi")
